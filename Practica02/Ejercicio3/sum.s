c0789774f7c222169889a83f7c183236bfcd8b24
antoniolm <codiak19@gmail.com>
2016-10-18 18:43:50 +0200

    Añadido el ejercicio3 casi completo

---
+++ Practica02/Ejercicio3/sum.s
@@ -0,0 +1,68 @@
+.section .data
+lista:
+	#.int  1,2,10, 1,2,0b10, 1,2,0x10
+	#.int 1,-1,1,1,-1,1,1,1,1
+	#.int -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1
+
+	.int 1,-2,1,-2,1,-2,1,-2,1,-2,1,-2,1,-2,1,-2,1,-2,1,-2,1,-2,1,-2,1,-2,1,-2,1,-2,1,-2
+	#.int 1,2,-3,-4,1,2,-3,-4,1,2,-3,-4,1,2,-3,-4,1,2,-3,-4,1,2,-3,-4,1,2,-3,-4,1,2,-3,-4
+longlista:
+	.int (.-lista)/4
+resultado:
+	.quad 0x01234567
+resto:
+	.quad 0x01234220
+
+formato:
+	.ascii "Cociente : %8d , Resto : %8d \n\0"
+
+.section .text
+main: .global main
+
+	mov $lista,    %ebx
+	mov longlista, %ecx
+	call suma
+	mov %edx, resto
+	mov %eax, resultado
+	
+	#pushl resultado+4
+	pushl resultado
+	#pushl resultado+4
+	pushl resultado
+	push $formato
+	call printf
+	add $12,%esp
+
+	mov $1, %eax
+	mov $0, %ebx
+	mov $0, %edi 
+
+	int $0x80
+
+
+suma:
+	push %edx
+	mov $0, %edx #mas   significativo sin acumular
+	mov $0, %eax #menos significativo sin acumular
+	mov $0, %ebp #contador del bucle
+	mov $0, %edi #mas   significativo acumulado
+	mov $0, %esi #menos significativo acumulado
+
+bucle:
+	mov (%ebx,%ebp,4),%eax
+	cdq  
+	add %eax,%esi
+	adc %edx,%edi
+	
+	inc %ebp
+	cmp %ebp,%ecx
+	jne bucle
+
+	# new
+	mov %esi,%eax
+	mov %edi,%edx
+	idiv %ecx
+
+	pop %edx
+	ret
+
